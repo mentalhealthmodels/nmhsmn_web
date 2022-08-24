15057b7267bc6c0da4a5f0851016ac7264cdcbbf
Matthew Hamilton <Matthew.Hamilton@orygen.org.au>
2022-08-24 21:48:09 +1000

    initial commit

---
+++ content/blog/categories-post.md
@@ -0,0 +1,15 @@
+---
+authors:
+- Sandra Diminc
+banner: img/banners/banner-4.jpg
+categories:
+- resources for modellers
+- resources for planners
+date: "2021-08-01T21:29:20+02:00"
+tags:
+- uses of modelling
+- types of models
+title: New modelling paper published.
+---
+
+Article about our first modelling paper.
---
+++ setup.R
@@ -0,0 +1,11 @@
+# 1. Created parent repository: https://github.com/mentalhealthmodels/nmhsmn_web
+# 2. Cloned repository to local machine
+# 3. Tried unsucesfully to create hugo site using blogdown::new_site function
+# 4. Instead:
+#     Created temporary directory
+#     Manually created Hugo site in temporary directory using RStudio New Project Wizard, as outlined at https://bookdown.org/yihui/blogdown/a-quick-example.html#create-project
+#     The Hugo theme supplied to the wizard : devcows/hugo-universal-theme
+#     Copied contents of temporary directory into cloned local github repository
+# 5. Tested successful installation of Hugo Universal theme by running 
+blogdown::serve_site()
+# 6. Began editing.
