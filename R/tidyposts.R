fb7f094f2f6161ccd2ea2b72d1f23541fe642e60
Matthew Hamilton <Matthew.Hamilton@orygen.org.au>
2022-09-01 13:28:58 +1000

    misc housecleaning

--- R/tidyposts.R
+++ R/tidyposts.R
@@ -1,13 +1,7 @@
-
 write_to_update_md_links <- function(fl_nm_1L_chr,
                                      path_to_post_dir_1L_chr,
                                      path_to_content_1L_chr = "content",
                                      post_dir_nm_1L_chr = character(0)){
-  # if(!identical(post_dir_nm_1L_chr, character(0))){
-  #   if(is.na(post_dir_nm_1L_chr)){
-  #     post_dir_nm_1L_chr <- fl_nm_1L_chr
-  #   }
-  # }
   file_path_1L_chr <- paste0(path_to_content_1L_chr,
                              "/",
                              path_to_post_dir_1L_chr,
@@ -31,17 +25,6 @@ write_to_update_md_links <- function(fl_nm_1L_chr,
                     text_chr)
   cat(text_chr, file=file_path_1L_chr, sep="\n")
 }
-# figs/
-# /blog/resources/modeller-resources/tutorials/managing_open_data/managing_open_data_files/figure-html/
-
-# path_to_content_dir_1L_chr = "content" 
-# post_dir_nm_1L_chr <- "managing_open_data"
-write_to_update_md_links(fl_nm_1L_chr = "Access_Open_Data",
-                         path_to_post_dir_1L_chr = "blog/resources/modeller-resources/tutorials/Managing-Open-Code-And-Data")
-
-##
-
-
 write_to_fix_ready4_in_md <- function(fl_nm_1L_chr,
                                       path_to_content_1L_chr,
                                       path_to_post_dir_1L_chr){
@@ -61,6 +44,10 @@ write_to_fix_ready4_in_md <- function(fl_nm_1L_chr,
         "",
         text_chr), file=path_to_md_1L_chr, sep="\n")
 }
-write_to_fix_ready4_in_md(fl_nm_1L_chr = "Access_Open_Data",
-                          path_to_post_dir_1L_chr = "blog/resources/modeller-resources/tutorials/Managing-Open-Code-And-Data",
-                          path_to_content_1L_chr = "content")
+## Call if problems rendering RMDs which make a library call to ready4
+# write_to_fix_ready4_in_md(fl_nm_1L_chr = "Access_Open_Data",
+#                           path_to_post_dir_1L_chr = "blog/resources/modeller-resources/tutorials/Managing-Open-Code-And-Data",
+#                           path_to_content_1L_chr = "content")
+## Call if rendering RMDs which create figures.
+# write_to_update_md_links(fl_nm_1L_chr = "Access_Open_Data",
+#                          path_to_post_dir_1L_chr = "blog/resources/modeller-resources/tutorials/Managing-Open-Code-And-Data")
